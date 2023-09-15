c7fee35caaed4ad3eb230fb85be9ac31ba9659ec
Md. Hasanur Rahman <hasanurhasu127@gmail.com>
2023-09-15 21:20:37 +0600

    added toast

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Course from "../Course/Course";
 import SelectedCourse from "../SelectedCourse/SelectedCourse";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
@@ -8,11 +10,17 @@ const Courses = () => {
     const [creditRemaining, setCreditRemaining] = useState(20);
     const [totalCredit, setTotalCredit] = useState(0);
     const [totalPrice, setTotalPrice] = useState(0)
+    const notify = () => toast("Wow so easy !");
 
     const handleSelectedCourse = (selectCourse, remainingCredit, creditTotal, price) => {
-        const newSelectedCourse =[...selectedCourse, selectCourse];
-        setSelectedCourse(newSelectedCourse);
 
+        const isExist = selectedCourse.find(item => item.id == selectCourse.id);
+        if (isExist) {
+            return toast.error('Already Selected')
+        } else {
+            const newSelectedCourse = [...selectedCourse, selectCourse];
+            setSelectedCourse(newSelectedCourse);
+        }
         const newCreditRemaining = creditRemaining - remainingCredit;
         setCreditRemaining(newCreditRemaining);
 
@@ -32,23 +40,35 @@ const Courses = () => {
         <div className="grid grid-cols-4 gap-5">
             <div className='grid grid-cols-3 gap-5 col-span-3'>
                 {
-                    courses.map(course => <Course 
-                        key={course.id} 
+                    courses.map(course => <Course
+                        key={course.id}
                         course={course}
                         handleSelectedCourse={handleSelectedCourse}
-                        ></Course>)
+                    ></Course>)
                 }
             </div>
 
             <div className="col-span-1">
-                <SelectedCourse 
-                selectedCourse={selectedCourse}
-                creditRemaining={creditRemaining}
-                totalCredit={totalCredit}
-                totalPrice={totalPrice}
+                <SelectedCourse
+                    selectedCourse={selectedCourse}
+                    creditRemaining={creditRemaining}
+                    totalCredit={totalCredit}
+                    totalPrice={totalPrice}
                 ></SelectedCourse>
-                
+
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
